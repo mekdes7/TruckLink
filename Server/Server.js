dd09b861e0a09d65173c9aa33158a4882136a2c7
Mekd <Mekd.tl12@gmail.com>
2025-08-07 01:23:49 +0300

    edit server

--- Server/Server.js
+++ Server/Server.js
@@ -10,6 +10,13 @@ const app = express();
 const PORT = process.env.PORT || 2000;
 app.use(cors());
 app.use(express.json());
+app.use(express.static("Client/build"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "Client", "build", "index.tsx"));
+});
+
+
 app.use('/api/manager',Managerrouter);
 app.use('/api/driver',Driverrouter);
 dbConfig.connect();
