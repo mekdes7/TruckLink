5b8653c68630d04bcf25714b01b091e118b661fd
Mekd <Mekd.tl12@gmail.com>
2025-08-07 01:37:12 +0300

    edit ser

--- Server/Server.js
+++ Server/Server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("Client/build"));
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "Client", "build", "index.tsx"));
 });
 
