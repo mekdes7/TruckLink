a7b9fa84155e83d382d572b80fe1797a0dd2a183
Mekd <Mekd.tl12@gmail.com>
2025-08-07 13:50:40 +0300

    server

--- Server/Server.js
+++ Server/Server.js
@@ -4,23 +4,34 @@ import dbConfig from './Config/DbConfig.js';
 import cors from 'cors';
 import Managerrouter from './Routes/managerRouter.js';
 import Driverrouter from './Routes/driverRoutes.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 2000;
+
 app.use(cors());
 app.use(express.json());
-app.use(express.static("Client/build"));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "Client", "build", "index.tsx"));
+// Serve static files from React build folder
+app.use(express.static(path.join(__dirname, 'Client', 'build')));
+
+
+app.use('/api/manager', Managerrouter);
+app.use('/api/driver', Driverrouter);
+
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Client', 'build', 'index.html'));
 });
 
 
-app.use('/api/manager',Managerrouter);
-app.use('/api/driver',Driverrouter);
 dbConfig.connect();
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
