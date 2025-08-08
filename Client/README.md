9c59c63e29023745ce7fd7abd87b6757b3831ff4
Mekd <Mekd.tl12@gmail.com>
2025-08-08 11:00:36 +0300

    update commit

--- Client/README.md
+++ Client/README.md
@@ -8,12 +8,11 @@ To run the frontend locally:
 The only requirement is having Node.js & npm installed and then run the following commands
 
 ```bash
-git clone https://github.com/melattgirmay/TruckLink-MVP.git
+git clone https://github.com/mekdes7/TruckLink
 cd TruckLink-MVP
 npm install
 npm run dev
 ```
-Then open your browser at:
-http://localhost:8080
 
-© 2025 Melat Girmay Hailu.
+
+
--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -3,10 +3,10 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(() => ({
+export default defineConfig({
   server: {
     host: "::",
-    port: 8080,
+    port: 2001,
   },
   plugins: [react()],
   resolve: {
@@ -14,4 +14,8 @@ export default defineConfig(() => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+  build: {
+    outDir: "dist",
+  },
+  base: "/", 
+});
